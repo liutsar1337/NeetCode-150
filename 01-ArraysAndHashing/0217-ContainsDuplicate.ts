38c702ca7ba119b97601be598f6b2900fd978361
liutsar <liutsar346@gmail.com>
2025-01-15 19:22:47 +0200

    Solve "217. Contains Duplicate" problem.

---
+++ 01-ArraysAndHashing/0217-ContainsDuplicate.ts
@@ -0,0 +1,13 @@
+function containsDuplicate(nums: number[]): boolean {
+  const numSet = new Set<number>();
+
+  for (let num of nums) {
+    if (numSet.has(num)) {
+      return true;
+    }
+    numSet.add(num);
+
+  }
+
+  return false;
+};
